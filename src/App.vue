5045f4f8f489b46807229f1588d1a4f11ee01f24
Dylan Corporon <dcorporon@goodmorning.com>
2024-02-21 11:39:36 -0700

    updated cards and layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col justify-center items-center h-screen">
+  <div class="flex flex-col justify-center items-center">
 
     <template v-if="isLoading">
       <div class="animate-pulse">
--- src/components/OrderResults.vue
+++ src/components/OrderResults.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-4"> <div v-for="recipe in recipes" :key="recipe.id" class="bg-white rounded-lg shadow-md p-4 border border-gray-200">
+      <img :src="recipe.image" alt="Placeholder Image" class="w-full h-40 object-cover mb-4 rounded-lg">
       <h2>{{ recipe.name }}</h2>
       <p>Cooking Time: {{ recipe.cookTimeMinutes }} minutes</p>
       </div>
