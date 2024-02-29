a393406958230ad5cd5020b22be79be942e856a8
Dylan Corporon <dcorporon@goodmorning.com>
2024-02-29 14:48:59 -0700

    update to the results

--- src/components/OrderResultsBox.vue
+++ src/components/OrderResultsBox.vue
@@ -1,18 +1,31 @@
 <template>
     <div>
-      <h2>Kachow</h2>
-        <div v-for="(order, orderIndex) in orderContents" :key="orderIndex">
-            <ul v-for="item in order.subItems" :key="item.sku">
-                    <li>{{ item.name }}</li>
-                    <li>{{ item.description }}</li>
-                    <li>{{ item.sku }}</li>
-                    <li>{{ item.status }}</li>
-            </ul>
+        <div class="container">
+
+            <div
+                v-if="orderContents && orderContents.length > 0"
+                :class="`columns-1 md:columns-2 lg:columns-${columnCount} mx-auto`"
+            >
+
+                <div v-for="(order, orderIndex) in orderContents" :key="orderIndex"
+                    class="bg-white p-4 rounded-md shadow-md min-w-2xl max-w-4xl"
+                >
+                    <ul>
+                        <li
+                            v-for="item in order.subItems" :key="item.sku"
+                        >
+                            {{ item.description }}
+                            <p>SKU: {{ item.sku }}</p>
+                            <p>Status: {{ item.status }}</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
+
     </div>
-
-
   </template>
+
 
 <script>
 export default {
@@ -32,7 +45,13 @@ export default {
     // Your component methods here
   },
   computed: {
-    // Your computed properties here
+    columnCount() {
+      if (this.orderContents && this.orderContents.length > 0) {
+        return this.orderContents.length;
+      } else {
+        return 1; // Default to a single column if no orders
+      }
+    }
   },
   mounted() {
     // Your component mounted hook logic here
